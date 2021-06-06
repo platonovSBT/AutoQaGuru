2b71c0f548f89b59a93de92f4d68ac02bba2fe62
Artem Platonov <artem.platonov@instamart.ru>
2021-06-06 19:20:59 +0400

    fix HW4

--- src/test/java/lesson4/HW_4.java
+++ src/test/java/lesson4/HW_4.java
@@ -1,11 +1,10 @@
 package lesson4;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HW_4 {
@@ -16,14 +15,12 @@ public class HW_4 {
         //в строке поиска найти Selenide
         $("[name=q]").setValue("selenide").pressEnter();
         //выбрать первый проект из списка
-        $(".f4.text-normal").$("a").click();
+        $("a[href=\"/selenide/selenide\"]").click();
         //перейти на вкладку WIKI
         $("[data-content=Wiki]").click();
         //проверка , что в списке страниц есть страница SoftAssertions
-        $(".Box-row.wiki-more-pages-link button").click();
-        $(".Box-row.wiki-more-pages a").shouldHave(Condition.text("SoftAssertions"));
-        //Открыть страницу SoftAssertions
-        $(".Box-row.wiki-more-pages a").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).click();
         // проверка, что внутри есть пример кода для JUnit5
         $$(".markdown-body li").shouldHave(itemWithText("Using JUnit5 extend test class:"));
 
@@ -34,11 +31,9 @@ public class HW_4 {
         // ***3. (опциональное) Запрограммируйте Drag&Drop с помощью Selenide.actions()
         // - Откройте https://the-internet.herokuapp.com/drag_and_drop
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        sleep(5000);
         // - Перенесите прямоугольник А на место В
         $("#column-a").dragAndDropTo("#column-b");
-        sleep(5000);
-        $("#column-a header").shouldHave(Condition.text("B"));
+        $("#column-a header").shouldHave(text("B"));
         // - Проверьте, что прямоугольники действительно поменялись
         //P.S. В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест,
         // если использовать её вместо actions()
